db8c9982795d20551b5257d12062590bb459e3d6
Naoya Fujio <naofudiwo@gmail.com>
2020-06-23 01:49:23 +0000

    apply near_station_validates

--- app/models/near_station.rb
+++ app/models/near_station.rb
@@ -1,4 +1,6 @@
 class NearStation < ApplicationRecord
     belongs_to :building
-    
+    validates :track_name, presence: true
+    validates :station_name, presence: true
+    validates :minute_walk, presence: true
 end
