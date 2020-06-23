b7f2fcb048bb194d29a3678cc5e3ed72ccabf4b8
Naoya Fujio <naofudiwo@gmail.com>
2020-06-23 01:58:24 +0000

    appdate_template

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,12 +1,15 @@
 class BuildingsController < ApplicationController
     def index
-        
+        @buildings = Building.all
     end
     def new
-        
+        @building = Building.new
     end
     def create
-        
+        @building = Building.new( #params )
+        if @property.save
+            redirect_to propaties_path
+        end
     end
     def show
         
