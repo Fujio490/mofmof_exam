118a91cd0e645141791cba329db60590ee5b4705
Naoya Fujio <naofudiwo@gmail.com>
2020-06-23 01:46:54 +0000

    apply building_validates

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,8 @@
 class Building < ApplicationRecord
-    has_many :buildings, dependent: :destroy
+    has_many :near_stations, dependent: :destroy
+    validates :name, presence: true
+    validates :rent, presence: true
+    validates :address, presence: true
+    validates :age, presence: true
+    validates :note, presence: true
 end
